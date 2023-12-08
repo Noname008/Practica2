6f6661c6be55322d88e1916192c4ab50cbb901ae
Noname008 <86607123+Noname008@users.noreply.github.com>
2023-12-08 15:23:05 +0400

    update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,14 +1,6 @@
 ﻿using System.Diagnostics;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Practica2
 {
@@ -17,13 +9,16 @@ namespace Practica2
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly Dictionary<Button, Func<long>> algorithms;
+        private readonly Dictionary<Button, Action<int, int>> algorithms;
         public MainWindow()
         {
             InitializeComponent();
-            algorithms = new Dictionary<Button, Func<long>>()
+            algorithms = new Dictionary<Button, Action<int, int>>()
             {
-                { B1, Algoritm.SequentialAlg }
+                { B1, Algoritm.AlgDataDecomposition },
+                { B2, Algoritm.AlgPrimesDecomposition },
+                { B3, Algoritm.AlgThreadPool },
+                { B4, Algoritm.AlgSequentialSearch },
             };
             foreach (var item in algorithms.Keys)
             {
@@ -37,50 +32,154 @@ namespace Practica2
             Content.Opacity = 0.3;
             Content.IsEnabled = false;
 
-            Task.Run(algorithms[(Button)sender].Invoke).ContinueWith(t =>
-            {
-                Load.Visibility = Visibility.Hidden;
-                Content.Opacity = 1;
-                Content.IsEnabled = true;
-                MessageBox.Show(t.Result.ToString());
-            }, TaskScheduler.FromCurrentSynchronizationContext());
+            if (Int32.TryParse(Threads.Text, out int threads) && Int32.TryParse(MaxValue.Text, out int maxValue))
+                Task.Run(() => Algoritm.TimeToAlg(algorithms[(Button)sender], threads, maxValue)).ContinueWith(t =>
+                {
+                    Load.Visibility = Visibility.Hidden;
+                    Content.Opacity = 1;
+                    Content.IsEnabled = true;
+                    MessageBox.Show(t.Result.ToString());
+                }, TaskScheduler.FromCurrentSynchronizationContext());
         }
     }
 
     public static class Algoritm
     {
-        private static List<Number> buffer = new List<Number>();
-        private static List<int> listprimes = new List<int>
-        {
-            2, 3, 5, 7
-        };
+        private static List<Number> buffer = [];
+        private static List<int> listprimes = [2, 3, 5, 7];
 
-        private static long BasicSequentialAlg(List<Number> numbers, params int[] primes)
+        private static void AddPrimes()
+        {
+            listprimes.AddRange(buffer
+                .Where((x) => x.Check == true)
+                .Select((x) => x.Value)
+                );
+        }
+
+        private static void BasicSequentialAlg(IEnumerable<Number> numbers, params int[] primes)
         {
             foreach(var item in numbers)
             {
-                if (primes.FirstOrDefault((x) =>
-                    {
-                        if (item.Value % x == 0)
-                            return true;
-                        return false;
-                    }, 0)
-                    != 0)
+                if (primes.FirstOrDefault((x) => item.Value % x == 0, 0) != 0)
                     item.Check = false;
             }
-
-
-            return 1;
         }
 
-        public static long SequentialAlg()
-        //test version for prime numbers 2,3,5,7 for the range of numbers from 2 to 100
+        private static void Clear()
         {
-            buffer.AddRange(Number.GetNumbers(25, 200));
-            BasicSequentialAlg(buffer, listprimes.ToArray());
-            int i = 0;
-            Debug.WriteLine(buffer[i].Value.ToString() + buffer[i].Check);
-            return 1;
+            buffer.Clear();
+            listprimes = [2, 3, 5, 7];
+        }
+
+        private static IEnumerable<IEnumerable<T>> Split<T>(IEnumerable<T> source, int count)
+        {
+            return source
+              .Select((x, y) => new { Index = y, Value = x })
+              .GroupBy(x => x.Index % count)
+              .Select(x => x.Select(y => y.Value).ToList())
+              .ToList();
+        }
+
+        public static long TimeToAlg(Action<int, int> f, int threads, int maxValue)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            f.Invoke(threads, maxValue);
+            sw.Stop();
+            //foreach(var item in listprimes)
+            //    Debug.WriteLine(item);
+            Clear();
+            return sw.ElapsedMilliseconds;
+        }
+
+        public static void AlgDataDecomposition(int threads, int maxValue)
+        {
+            int i;
+            do
+            {
+                i = (int)(Math.Pow(listprimes.Last(), 2) < maxValue ? Math.Pow(listprimes.Last(), 2) : maxValue);
+                buffer = new List<Number>(Number.GetNumbers(listprimes.Last(), i));
+                var list = Split(buffer, threads);
+                Parallel.For(0, threads, (x) => BasicSequentialAlg(
+                    list.ToArray()[x], 
+                    [.. listprimes]));
+                AddPrimes();
+            } while (i < maxValue);
+        }
+
+        public static void AlgPrimesDecomposition(int threads, int maxValue)
+        {
+            int validThreads;
+            int i;
+            do
+            {
+                if (listprimes.Count > threads)
+                    validThreads = threads;
+                else
+                    validThreads = listprimes.Count;
+
+                i = (int)(Math.Pow(listprimes.Last(), 2) < maxValue ? Math.Pow(listprimes.Last(), 2) : maxValue);
+                buffer = new List<Number>(Number.GetNumbers(listprimes.Last(), i));
+                var list = Split(listprimes.ToArray(), validThreads);
+                Parallel.For(0, validThreads, x => BasicSequentialAlg(
+                    buffer,
+                    [.. list.ToArray()[x]]));
+                AddPrimes();
+            } while (i < maxValue);
+        }
+
+        public static void AlgThreadPool(int threads, int maxValue)
+        {
+            int i;
+            object tasksLock = new();
+            int tasks = 0;
+            do
+            {
+                i = (int)(Math.Pow(listprimes.Last(), 2) < maxValue ? Math.Pow(listprimes.Last(), 2) : maxValue);
+                buffer = new List<Number>(Number.GetNumbers(listprimes.Last(), i));
+                foreach(int prime in listprimes)
+                {
+                    while (tasks >= threads) { }
+                    lock (tasksLock)
+                    {
+                        tasks++;
+                    }
+                    Task.Run(() => BasicSequentialAlg(buffer, prime)).ContinueWith((t) =>
+                    {
+                        lock(tasksLock)
+                        {
+                            tasks--;
+                        }
+                    });
+                }
+                while (tasks > 0) { }
+                AddPrimes();
+            } while (i < maxValue);
+        }
+
+        public static void AlgSequentialSearch(int threads, int maxValue)
+        {
+            int i;
+            object primeLock = new();
+            int indexPrime = 0;
+            do
+            {
+                i = (int)(Math.Pow(listprimes.Last(), 2) < maxValue ? Math.Pow(listprimes.Last(), 2) : maxValue);
+                buffer = new List<Number>(Number.GetNumbers(listprimes.Last(), i));
+                Parallel.For(0, threads, x =>
+                {
+                    int thisPrime;
+                    while(indexPrime < listprimes.Count)
+                    {
+                        lock (primeLock)
+                        {
+                            thisPrime = indexPrime++;
+                        }
+                        BasicSequentialAlg(buffer, listprimes[thisPrime]);
+                    }
+                });
+                AddPrimes();
+                indexPrime = 0;
+            } while (i < maxValue);
         }
     }
 
@@ -96,11 +195,7 @@ namespace Practica2
 
         public static IEnumerable<Number> GetNumbers(int start, int end)
         {
-            return new Number[end - start].Select((t, i) =>
-            {
-                t = new Number(i + start);
-                return t;
-            });
+            return new Number[end - start].Select((t, i) => t = new Number(i + start));
         }
     }
 }
